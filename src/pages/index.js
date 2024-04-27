acdbab1cbab1e7bb47676e572c40b6b27c27c76e
Olga Barbato <olga.barbato@gmail.com>
2024-04-27 18:35:08 +0200

    Modal con logica per post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.scss";
+import Modal from "@/components/modal";
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <Modal />
+      </main>
     </>
   );
 }
